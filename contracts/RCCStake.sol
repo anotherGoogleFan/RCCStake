4541fc5e2d0918d60810fe1196569be42bc7a5ca
Chen Yongren <mac@k-Pro-2.local>
2024-08-12 21:41:54 +0700

    .

--- contracts/RCCStake.sol
+++ contracts/RCCStake.sol
@@ -2,7 +2,23 @@
 pragma solidity 0.8.26;
 
 contract RCCStake {
-    constructor() {
-        
+    // contract owner
+    address private owner;
+    // map[chainID]address
+    mapping (uint256 => address) private ChainAddr;
+    // map[chainID]weight
+    mapping (uint256 => uint256) private ChainWeight;  
+    // map[chainID]minDepositAmount
+    mapping (uint256 => uint256) ChainMinDepositAmount;
+    // map[chainID]map[user_address]balance
+    mapping (uint256 => mapping (address => uint256)) private balance;
+  
+    modifier onlyOwner{
+        require(msg.sender == owner, "only owner allowed");
+        _;
+    }
+
+    constructor(address _owner) {
+        owner = _owner;
     }
 }
